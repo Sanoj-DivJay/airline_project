218658c0cdfb569d2404c40671cd0b54410145a6
Sanudi Pinnawala <sanudisarithma1999@gmail.com>
2022-10-06 01:46:52 +0530

    Update 1 contactus java class

--- src/main/java/contactpackage/contactus.java
+++ src/main/java/contactpackage/contactus.java
@@ -1,5 +1,14 @@
 package contactpackage;
 
-public class contactus {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-}
+public class contactus
+{
+	Statement st;
+	public void addcontactus(String name, String email, String message) throws SQLException
+	{
