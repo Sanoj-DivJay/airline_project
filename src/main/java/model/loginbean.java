541c4a2ace8f6181a0bab9e72dc898505891861f
Chirath Kavindu <95146759+Chira2000@users.noreply.github.com>
2022-10-07 12:46:54 +0530

    loginbean

---
+++ src/main/java/model/loginbean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+
+public class loginbean {
+    private String name;
+    
+    public void setname(String name){
+    this.name=name;
+    }
+    
+    public String getname(){
+    return name;
+    }
+}
