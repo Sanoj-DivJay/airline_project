18edda9a66e300a445376f979b7db124a789317f
Sanoj Divakara <sanojdivjay@gmail.com>
2022-10-07 12:31:51 +0530

    user update servlet

---
+++ src/main/java/controller/updateuser.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.List;
+import model.dbconnect;
+
+
+public class updateuser extends HttpServlet {
+
+  
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet updateuser</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet updateuser at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+  
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       // processRequest(request, response);
+       
+        PrintWriter out=response.getWriter();
+        String username =request.getParameter("username");
+        String fullname =request.getParameter("fullname");
+        String email =request.getParameter("email");
+      
+        
+     
+        
+             try{
+         dbconnect con=new dbconnect();
+         boolean rslt=con.updateuserdetails(username,fullname,email);
+         if (rslt==true){
+         response.sendRedirect("dashboardstaff_01.jsp"); 
+         out.println("Reg Successfull");
+         }
+         else
+            out.println("reg unsuccessfull"); 
+         response.sendRedirect("dashboardstaff_01.jsp"); 
+         
+         }
+         catch(Exception e)
+         {
+             e.printStackTrace();
+         }
+    }
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
